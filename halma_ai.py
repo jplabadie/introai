20ec8b0ff2a4d325136365af4aa2eb3a51b8a90f
jplabadie <jhl42@nau.edu>
2017-05-09 10:41:36 -0700

    Minor edit

--- halma_ai.py
+++ halma_ai.py
@@ -1,11 +1,9 @@
 import math
 from collections import namedtuple
 
-mm_tree = namedtuple('Tree', ['state', 'score', 'children', 'move'])
+def __init__(self,core):
+    self.halma_core = core
 
-def __init__(self):
-    self.centerline = [(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7)]
-    
 # return distance to goal from current (manhattan) given tuple coords
 def getGoalDist(self, cur_pos, player):
     distance = float('inf')
@@ -33,9 +31,40 @@ def getCenterlineDist(self, cur_pos):
 def getBoardValue(self, player):
     value = 0
     for pawn in player.pawns:
-        pawn_position_val = getCenterlineDist( pawn )
-        pawn_position_val + getGoalDist( pawn, player.goal)
+        pawn_position_val = self.getCenterlineDist( pawn )
+        pawn_position_val + self.getGoalDist( pawn, player.goal)
         value = value + pawn_position_val
     return value
 
+#return a namedtuple implementation of a minimax tree with the given params
+def getMMTree(self, depth, friendly, opfor, node_max, team_info, opp_info, move, prune, alpha, evalFunc):
+    mm_tree = namedtuple('Tree', ['state', 'score', 'children', 'move'])
+    children = []
+    
+    if node_max:
+        teams = (friendly, opfor)
+        minimax = max
+        compVal = -1
+        score = 0
+    else:
+        teams = (opfor, friendly)
+        minimax = min
+        compVal = 1
+        score = float('inf')
+
+    if not depth == 0:
+        for src in friendly:
+            for dest in self.board.get_valid_moves(src, team_info):
+                teamC = friendly.copy()
+                self.halma_core.sub_move(dest, src, team_info, teamC)
+                child = self.getMMTree(depth - 1, opfor, teamC, not node_max, opp_info,
+                                      team_info, (src, dest), prune, score, evalFunc)
+                children.append(child)
+                score = minimax(score, child.score)
+                if prune and self.compare(score, alpha) == compVal:
+                    return mm_tree(teams, score, children, move)
+    else:
+        score = evalFunc(team_info, friendly, opfor) if node_max else evalFunc(opp_info, opfor, friendly)
+
+        return mm_tree(teams, score, children, move)
 
