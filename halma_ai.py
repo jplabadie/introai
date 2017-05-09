caf652402077bde881b7a04a90762d4f3ca3633d
jplabadie <jhl42@nau.edu>
2017-05-08 18:03:56 -0700

    Add distance function

--- halma_ai.py
+++ halma_ai.py
@@ -1,6 +1,11 @@
+
+# return distance to goal from current (manhattan) given tuple coords
 def getGoalDist(self, cur_pos, targ_zone):
     dist = float('inf')
     for target in targ_zone:
-        goal_dist = ((targ_zone[0]-cur_pos[0])**2)
-        goal_dist = goal_dist + targ_zone[1]-cur_pos[1]**2
-        goal_dist
+        goal_dist = ((target[0]-cur_pos[0])**2)
+        goal_dist = goal_dist + target[1]-cur_pos[1]**2
+        goal_dist = goal_dist**(1/2)
+
+    return min(dist,goal_dist)
+
