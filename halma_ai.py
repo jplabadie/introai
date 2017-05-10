86ada303692d40e01cfd522a338b3418fb4feade
jplabadie <jhl42@nau.edu>
2017-05-09 19:05:10 -0700

    fix and finalize ai

--- halma_ai.py
+++ halma_ai.py
@@ -1,20 +1,29 @@
 import math
 from collections import namedtuple
+#from numpy import linalg
+#from scipy.spatial import distance
 
 class HalmaAI( object ):
 
     def __init__(self,core):
         self.halma_core = core
+        self.fbmode = False
 
     # return distance to goal from current (manhattan) given tuple coords
     def getGoalDist(self, cur_pos, player_goal):
         distance = float('inf')
         for target in player_goal:
             goal_dist = ((target[0]-cur_pos[0])**2)
-            goal_dist = goal_dist + target[1]-cur_pos[1]**2
+            goal_dist = goal_dist + (target[1]-cur_pos[1])**2
             goal_dist = goal_dist**(1/2)
             distance = min(distance,abs(goal_dist))
         return distance
+
+    def getSumOfSquares(self, from_node, to_node):
+        sum = 0
+        for node in from_node:
+            sum = sum + (((to_node[0] -node[0]) ** 2) + ((to_node[1] - node[1])) ** 2)
+        return sum
 
     # return the distance to the centerline (nearest centerline coord)
     # see 'Another Formula' https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line)
@@ -29,12 +38,20 @@ class HalmaAI( object ):
         distance = numerator / denominator
         return distance
 
+    def getLineDist(self, cur_pos):
+        min = 100
+        line = [(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7)]
+        for center in line:
+            cur = 0#distance.euclidean(cur_pos,center)
+            if cur<min:
+                min = cur
+
     # evaluate a given board for a given player
     def getBoardValue(self, player):
         value = 0
         for pawn in player["pawns"]:
-            pawn_position_val = self.getCenterlineDist( pawn )
-            pawn_position_val + self.getGoalDist( pawn, player["goal"])
+            #pawn_position_val = self.getCenterlineDist( pawn )
+            pawn_position_val = (8-self.getGoalDist( pawn, player["goal"]))
             value = value + pawn_position_val
         return value
 
@@ -56,18 +73,32 @@ class HalmaAI( object ):
 
         if not depth == 0:
             for from_pos in cur_player["pawns"]:
-                for to_pos in self.halma_core.findMoves(from_pos):
-                    cur_player_cpy = cur_player.copy()
-                    self.halma_core.move(cur_player_cpy, from_pos, to_pos)
+                for to_pos in self.halma_core.findMoves(from_pos,cur_player):
+
+                    cpy_goal = cur_player["goal"].copy()
+                    cpy_pawns =  cur_player["pawns"].copy()
+                    cpy_player = cur_player["player"]
+                    cpy_name = cur_player["name"]
+
+                    cur_player_cpy = {"goal":cpy_goal,"pawns":cpy_pawns,"player":cpy_player,"name":cpy_name}
+
+                    self.halma_core.exploreMove(cur_player_cpy, from_pos, to_pos)
                     child = self.getMMTree(depth - 1, op_player, cur_player_cpy, not node_max,
                                            (from_pos, to_pos), prune, score)
+
                     children.append(child)
+
                     score = minimax(score, child.score)
-                    if prune and ((score>alpha)-(alpha>score) == compVal):
+                    ab = (score > alpha) - (alpha > score)
+                    #print( "!"+str(ab)+":"+str(compVal) + ":"+str(minimax))
+                    #print(prune)
+                    if prune and ab == compVal:
+                        print("\n\n#$%@%$%#%#%Q@#%@$#% PRUNED #%$%^%$^#\n\n")
                         return mm_tree(teams, score, children, move)
         else:
             score = self.getBoardValue(cur_player) if node_max else self.getBoardValue(op_player)
 
+        #print(score)
         return mm_tree(teams, score, children, move)
 
     # return a best move (as a tuple coord pair) given parameters, using the minimaxTree
@@ -76,10 +107,42 @@ class HalmaAI( object ):
         score = math.inf
         if max_node:
             score = 0
-
+        if self.fbmode:
+            print("FBMODE")
+            return self.fbmove(cur_player)
         mm_tree = self.getMMTree(depth, cur_player, op_player, max_node, (), ab_prune, score)
 
         for child in mm_tree.children:
             if child.score == mm_tree.score:
+                if self.fbmode:
+                    print("FBMODE")
+                    return self.fbmove(cur_player)
+
+               # print("highest: "+str(child.score))
+                #print(child)
                 return child.move
-        return ((0, 0), (0, 0))
+        return False
+
+    def fbmove(self, player):
+        moves = self.halma_core.findAllMoves(player)
+        best_move = None
+        min = 100
+
+        if player["name"] == "green":
+            p_goal = (7,7)
+        else:
+            p_goal = (0,0)
+
+        print("Moves available: " + str(moves))
+        for move in moves:
+            c_val = self.getCenterlineDist(move)
+            g_val = self.getGoalDist(move,player["goal"])
+            tot_val = c_val + g_val
+            print("move: "+ str(move)+ " totval: "+ str(tot_val)+" lineval: " + str(c_val) + " goalval: "+ str(c_val))
+
+            if(tot_val<min):
+                min = tot_val
+                best_move = move
+        print("BestbyFB: "+ str(best_move))
+        return best_move
+
